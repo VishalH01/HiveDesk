fc195f462f0cd1799d4f87f3737dcf2c9e5efbd1
VishalH01 <vishal.s.haramkar@gmail.com>
2025-07-10 00:07:00 +0530

    .

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const app = express();
 connectDB();
 
 // CORS configuration - MUST come before other middleware
-const corsOrigin = (process.env.CORS_ORIGIN || 'http://localhost:5173').replace(/\/$/, '');
+const corsOrigin = (process.env.CORS_ORIGIN).replace(/\/$/, '');
 app.use(cors({
   origin: corsOrigin,
   credentials: true,
